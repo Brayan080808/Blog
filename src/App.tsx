578ac4a54d607da89639b6ed77c3c3dae81bc3e3
AndresT0590 <andres0590tovar@gmail.com>
2025-01-16 18:24:55 -0500

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
+import loginImg from '/loginImg.png'
+
 
 function App() {
-
+  // A39DFF
+  //625E99
   return (
     <>
-      <div className='w-full h-full'>
-        <div className=" bg-gray-100 border border-gray-50 shadow-lg w-[40%] h-[75%]">
-          <h2>Blog</h2>
-          <form></form>
+      <div className='overflow-hidden w-screen h-screen '>
+        <div className='w-[30%] h-full flex flex-col justify-between text-white bg-gradient-to-r from-[#625E99] to-[#A39DFF] pl-8 pr-4 pb-4 py-8' >
+
+          <div className=' w-full  mb-6 pr-20 pl-8'>
+            <h2 className='text-3xl my-4 font-semibold'>Welcome to  
+            <span className='font-bold'> Total <br /> Security System!
+              </span></h2>
+            <h2></h2>
+            <p className=' text-xs'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aut assumenda accusantium vre 
+            </p>
+
+          </div>
+
+        <div className='  mt-6'>
+          <img src={loginImg} alt="" className=' object-cover' />
 
         </div>
-      </div>
-   
 
+
+
+
+
+
+
+
+        </div>
+
+
+
+
+      </div>
     </>
   )
 }
