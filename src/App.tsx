9c21f2fbf2dd2cc0f6152fdca3ef2991b2ce3df8
AndresT0590 <andres0590tovar@gmail.com>
2025-01-16 15:56:21 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+function App() {
+
+  return (
+    <>
+      <div className='w-full h-full'>
+        <div className=" bg-gray-100 border border-gray-50 shadow-lg w-[40%] h-[75%]">
+          <h2>Blog</h2>
+          <form></form>
+
+        </div>
+      </div>
+   
+
+    </>
+  )
+}
+
+export default App
